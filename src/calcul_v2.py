263fa24929166f095f28a030dcc3465dd823fdf7
Adrien Vergnaud <a.vergnaud@gmail.com>
2018-05-03 20:18:28 +0200

    exemple de TU

--- src/calcul_v2.py
+++ src/calcul_v2.py
@@ -1,7 +1,5 @@
 class Calcul:
-    def generateURL(self, ticker, days, period):
-        return "https://www.google.com/finance/getprices?i=" + str(period) + "&p=" + str(days) + "d&f=d,o,h,l,c,v&df=cpct&q=" + ticker
-
+    
     def traduitSecondes(self, secondes):
 
         diviseurs = [365*24*60*60, 24*60*60, 60*60, 60, 1]
